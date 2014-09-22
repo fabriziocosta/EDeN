f198e6beb6f2ae08902bb9cd991373d98eb7c0c4
Fabrizio Costa <xfcosta@gmail.com>
2014-09-22 19:02:34 +0200

    rest update

--- eden/converters/obabel.py
+++ eden/converters/obabel.py
@@ -1,21 +1,21 @@
-import pybel as pb
 import openbabel as ob
+import pybel as pb
 import networkx as nx 
 
-def obabel_to_eden(infile, file_type='smi'):
+def obabel_to_eden(infile, file_type='sdf'):
     
     def obabel2networkx(mol):
         g = nx.Graph()
         for atom in mol:
             vlabel=atom.type
-            hvlabel=[hash(vlabel)]
+            hvlabel=[hash(str(atom.atomicnum))]
             g.add_node(atom.idx, label=vlabel, hlabel=hvlabel)
 
             edges = []
         bondorders = []
         for bond in ob.OBMolBondIter(mol.OBMol):
             elabel=bond.GetBO()
-            helabel=[hash(elabel)]
+            helabel=[hash(str(elabel))]
             g.add_edge(bond.GetBeginAtomIdx(),bond.GetEndAtomIdx(), label=elabel, hlabel=helabel)
         return g
 
