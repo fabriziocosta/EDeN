02eb3c47286254b91c7f5d6b66f676873e78d53f
Fabrizio Costa <xfcosta@gmail.com>
2014-10-08 11:39:29 +0200

    new matrix code. also distances are supported

---
+++ eden/bin/matrix.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import logging
+
+from sklearn import metrics
+
+from eden import graph
+from eden.converters import dispatcher
+from eden.util import argument_parser, setup, eden_io
+
+DESCRIPTION = """
+Explicit Decomposition with Neighborhood Utility program.
+Pairwise matrix computation.
+"""
+
+def setup_parameters(parser):
+	parser = argument_parser.setup_common_parameters(parser)
+	parser.add_argument("-t", "--type",  choices = ["similarity","distance"],
+    	help = "", 
+    	default = "similarity")
+	parser.add_argument("--distance-metric",  choices = ["cityblock", "cosine", "euclidean", "l1", "l2", "manhattan"],
+    	dest = "distance_metric",
+    	help = """Note that in the case of ‘cityblock’, ‘cosine’ and ‘euclidean’ 
+    	(which are valid scipy.spatial.distance metrics), the scikit-learn implementation 
+    	will be used, which is faster and has support for sparse matrices 
+    	(except for ‘cityblock’).""", 
+    	default = "euclidean")
+	parser.add_argument("--similarity-metric",  choices = ["rbf", "sigmoid", "polynomial", "poly", "linear", "cosine"],
+    	dest = "similarity_metric",
+    	help = """Valid metrics.""", 
+    	default = "linear")
+	return parser
+
+
+def main(args):
+	"""
+	Pairwise matrix computation.
+	"""
+
+	#input data
+	g_it = dispatcher.any_format_to_eden(input_file = args.input_file, format = args.format)	
+	vec = graph.Vectorizer(r = args.radius,d = args.distance, nbits = args.nbits)
+	X = vec.transform(g_it, n_jobs = args.n_jobs)
+	logging.info('Instances: %d Features: %d with an avg of %d features per instance' % (X.shape[0], X.shape[1],  X.getnnz()/X.shape[0]))
+
+	#compute pairwise matrix
+	if args.type == "similarity":
+		K = metrics.pairwise.pairwise_kernels(X, metric = args.similarity_metric, n_jobs = args.n_jobs)
+	elif args.type == "distance":
+		K = metrics.pairwise.pairwise_distances(X, metric = args.distance_metric, n_jobs = args.n_jobs)
+	#output
+	eden_io.store_matrix(matrix = K, output_dir_path = args.output_dir_path, out_file_name = "matrix", output_format = args.output_format)
+
+
+if __name__  == "__main__":
+	args = setup.setup(DESCRIPTION, setup_parameters)
+	logging.info('Program: %s' % sys.argv[0])
+	logging.info('Started')
+	main(args)
+	logging.info('Finished')
