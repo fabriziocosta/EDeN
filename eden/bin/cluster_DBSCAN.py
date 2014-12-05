f8f97ec92c2a72cac229066e26d4da98372d5561
Fabrizio Costa <xfcosta@gmail.com>
2014-12-05 12:26:58 +0100

    logging tracebacks

--- eden/bin/cluster_DBSCAN.py
+++ eden/bin/cluster_DBSCAN.py
@@ -77,6 +77,12 @@ if __name__  == "__main__":
 	logger.info('-'*80)
 	logger.info('Program: %s' % sys.argv[0])
 	logger.info('Parameters: %s' % args.__dict__)
-	cluster_DBSCAN(args)
-	end_time = time.clock()
-	logger.info('Elapsed time: %.1f sec',end_time - start_time)
+	try:
+		cluster_DBSCAN(args)
+	except Exception:
+		import datetime
+		curr_time = datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
+		logger.exception("Program run failed on %s" % curr_time)
+	finally:
+		end_time = time.clock()
+		logger.info('Elapsed time: %.1f sec',end_time - start_time)	
