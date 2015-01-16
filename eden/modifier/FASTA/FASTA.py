2231c82482e2c2ee83758f300997dbce2f7613bc
Fabrizio Costa <xfcosta@gmail.com>
2015-01-16 16:20:52 +0100

    started FASTA refactoring

---
+++ eden/modifier/FASTA/FASTA.py
@@ -0,0 +1,47 @@
+import random
+
+def FASTA_to_FASTA(input = None, input_type = None):
+    """
+    Takes a FASTA file yields a normalised FASTA file.
+
+    Parameters
+    ----------
+    input : string
+        A pointer to the data source.
+
+    input_type : ['url','file','string_file']
+        If type is 'url' then 'input' is interpreted as a URL pointing to a file.
+        If type is 'file' then 'input' is interpreted as a file name.
+        If type is 'list' then 'input' is interpreted as a list of strings.
+    """
+    input_types = ['url','file','list']
+    assert(input_type in input_types),'ERROR: input_type must be one of %s ' % input_types
+
+    if input_type == 'file':
+        f = open(input,'r')
+    elif input_type == 'url':
+        import requests
+        f = requests.get(input).text.split('\n')
+    elif input_type == "list":
+        f = input
+    return _FASTA_to_FASTA(f)        
+
+
+def _FASTA_to_FASTA(data_str_list):
+    line_buffer = ''
+    for line in data_str_list:
+        _line = line.strip().upper()
+        if _line:
+            if _line[0] == '>':
+                #extract string from header
+                header_str = _line[1:] 
+                if len(line_buffer) > 0:
+                    yield '>' + prev_header_str
+                    yield line_buffer
+                line_buffer = ''
+                prev_header_str = header_str
+            else:
+                line_buffer += _line
+    if len(line_buffer) > 0:
+        yield '>' + prev_header_str
+        yield line_buffer
