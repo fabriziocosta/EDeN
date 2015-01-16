6f6e0c0af19ca02f0f1d40b2f3fca052446d36db
Fabrizio Costa <xfcosta@gmail.com>
2015-01-16 16:32:10 +0100

    FASTA refactoring

--- eden/modifier/FASTA/FASTA_insert_landmark.py
+++ eden/modifier/FASTA/FASTA_insert_landmark.py
@@ -1,17 +1,13 @@
-import random
 from eden.modifier.FASTA import FASTA
 
 def FASTA_insert_landmark_to_FASTA(input = None, input_type = None, **options):
-    return _FASTA_insert_landmark_to_FASTA(FASTA.FASTA_to_FASTA(input=input,input_type=input_type), **options)        
-
-
-def _FASTA_insert_landmark_to_FASTA(data_str_list, **options):
     landmark_relative_position = options.get('landmark_relative_position',0.5)
     landmark_char =  options.get('landmark_char','@')
-    for line in data_str_list:
-        header_str = line
-        line_buffer = data_str_list.next()
-        pos = int( len(line_buffer) * landmark_relative_position )
-        seq = line_buffer[:pos] + landmark_char + line_buffer[pos:]
-        yield '>' + header_str
-        yield seq
+    lines = FASTA.FASTA_to_FASTA(input = input, input_type = input_type)
+    for line in lines:
+        header = line
+        seq = lines.next()
+        pos = int( len(seq) * landmark_relative_position )
+        seq_mod = seq[:pos] + landmark_char + seq[pos:]
+        yield '>' + header
+        yield seq_mod
