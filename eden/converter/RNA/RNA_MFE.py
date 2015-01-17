c3fad0368febffafb72b53a6e10fe5fdb294c954
Fabrizio Costa <xfcosta@gmail.com>
2015-01-17 16:05:09 +0100

    FASTA refactoring

--- eden/converter/RNA/RNA_MFE.py
+++ eden/converter/RNA/RNA_MFE.py
@@ -47,4 +47,5 @@ def RNA_MFE_to_eden(input = None, input_type = None, **options):
         seq = lines.next()
         G = string_to_networkx(seq, **options)
         G.graph['ID'] = header
-        yield G
+        if G.number_of_nodes() > 0 :
+            yield G
