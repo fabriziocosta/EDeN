8493b8f11fd0f20da07efed1758aac5c9ef14dbc
Fabrizio Costa <xfcosta@gmail.com>
2015-01-27 14:02:22 +0100

    generalized contract

--- eden/modifier/RNA/vertex_attributes.py
+++ eden/modifier/RNA/vertex_attributes.py
@@ -10,30 +10,3 @@ def add_paired_unpaired_vertex_type(graph_list = None, output_attribute = 'type'
 				if ed['type'] == 'basepair':
 					g.node[n][output_attribute] = 'paired'
 		yield g
-
-
-def add_vertex_type(graph_list = None, output_attribute = 'type', separator = '', level = 1):
-	"""
-		level: int
-			level = 1 considers all incident edges
-
-			level = 2 considers all edges incident on the neighbors  
-	"""
-	for g in graph_list:
-		#iterate over nodes
-		for n, d in g.nodes_iter(data = True):
-			#for all neighbors
-			edge_labels = []
-			if level == 1 :
-				edge_labels += [ed.get('label','N/A') for u,v,ed in g.edges_iter(n, data = True)]
-			elif level == 2 :
-				neighbors = g.neighbors(n)
-				for nn in neighbors:
-					#extract list of edge labels
-					edge_labels += [ed.get('label','N/A') for u,v,ed in g.edges_iter(nn, data = True)]
-			else :
-				raise Exception('Unknown level: %s' % level)
-			#consider the sorted serialization of all labels as a type
-			vertex_type = separator.join(sorted(edge_labels))
-			g.node[n][output_attribute] = vertex_type
-		yield g
--- eden/modifier/graph/vertex_attributes.py
+++ eden/modifier/graph/vertex_attributes.py
@@ -2,6 +2,60 @@ import networkx as nx
 import numpy as np 
 
 
+def incident_edge_label(graph_list = None, output_attribute = 'type', separator = '', level = 1):
+	"""
+		level: int
+			level = 1 considers all incident edges
+
+			level = 2 considers all edges incident on the neighbors  
+	"""
+	for g in graph_list:
+		#iterate over nodes
+		for n, d in g.nodes_iter(data = True):
+			#for all neighbors
+			edge_labels = []
+			if level == 1 :
+				edge_labels += [ed.get('label','N/A') for u,v,ed in g.edges_iter(n, data = True)]
+			elif level == 2 :
+				neighbors = g.neighbors(n)
+				for nn in neighbors:
+					#extract list of edge labels
+					edge_labels += [ed.get('label','N/A') for u,v,ed in g.edges_iter(nn, data = True)]
+			else :
+				raise Exception('Unknown level: %s' % level)
+			#consider the sorted serialization of all labels as a type
+			vertex_type = separator.join(sorted(edge_labels))
+			g.node[n][output_attribute] = vertex_type
+		yield g
+
+
+def incident_node_label(graph_list = None, output_attribute = 'type', separator = '', level = 1):
+	"""
+		level: int
+			level = 1 considers all incident nodes
+
+			level = 2 considers all nodes incident on the neighbors  
+	"""
+	for g in graph_list:
+		#iterate over nodes
+		for n, d in g.nodes_iter(data = True):
+			#for all neighbors
+			node_labels = []
+			if level == 1 :
+				node_labels += [g.node[u].get('label','N/A') for u in g.neighbors(n)]
+			elif level == 2 :
+				neighbors = g.neighbors(n)
+				for nn in neighbors:
+					#extract list of labels
+					node_labels += [g.node[u].get('label','N/A') for u in g.neighbors(nn)]
+			else :
+				raise Exception('Unknown level: %s' % level)
+			#consider the sorted serialization of all labels as a type
+			vertex_type = separator.join(sorted(node_labels))
+			g.node[n][output_attribute] = vertex_type
+		yield g
+
+
 def translate(graph_list = None, input_attribute = 'label', output_attribute = 'label', label_map = dict(), default = ' '):
 	for g in graph_list:
 		#iterate over nodes
