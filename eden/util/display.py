dfdac5b8575aa553ae94950308c51baff9095868
Fabrizio Costa <xfcosta@gmail.com>
2015-02-01 19:47:16 +0100

    added embed2D

--- eden/util/display.py
+++ eden/util/display.py
@@ -127,3 +127,67 @@ def serialize_graph( graph ):
     json_data  =  json_graph.node_link_data( graph )
     serial_data  =  json.dumps( json_data, separators  =  ( ',',':' ), indent  =  4, cls  =  SetEncoder )
     return serial_data
+
+
+def embed2D(input, vectorizer, labels = None, size = 10, n_components = 5, gamma = 20, nu = 0.01, n_jobs = 1):
+    import numpy as np
+
+    #transform input into sparse vectors
+    X = vectorizer.transform( input , n_jobs = n_jobs )
+    
+    #embed high dimensional sparse vectors in 2D
+    from sklearn import metrics
+    D = metrics.pairwise.pairwise_distances( X )
+
+    from sklearn.manifold import MDS
+    feature_map = MDS( n_components=n_components, dissimilarity='precomputed')
+    X_explicit=feature_map.fit_transform( D )
+
+    from sklearn.decomposition import TruncatedSVD
+    pca = TruncatedSVD( n_components = 2 )
+    X_reduced = pca.fit_transform( X_explicit )
+
+    plt.figure(figsize=(size,size))
+
+    #make mesh
+    x_min, x_max = X_reduced[:, 0].min(), X_reduced[:, 0].max()
+    y_min, y_max = X_reduced[:, 1].min(), X_reduced[:, 1].max()
+    step_num = 50
+    h = min( ( x_max - x_min ) / step_num  , ( y_max - y_min ) / step_num )# step size in the mesh
+    b = h * 10 # border size
+    x_min, x_max = X_reduced[:, 0].min() - b, X_reduced[:, 0].max() + b
+    y_min, y_max = X_reduced[:, 1].min() - b, X_reduced[:, 1].max() + b
+    xx, yy = np.meshgrid( np.arange( x_min, x_max, h ), np.arange( y_min, y_max, h ) )
+
+    #induce a one class model to estimate densities
+    from sklearn.svm import OneClassSVM
+    clf = OneClassSVM( gamma = gamma, nu = nu )
+    clf.fit( X_reduced )
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, m_max] . [y_min, y_max].
+    if hasattr(clf, "decision_function"):
+        Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    else:
+        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+    # Put the result into a color plot
+    levels = np.linspace(min(Z), max(Z), 40)
+    Z = Z.reshape(xx.shape)
+
+    plt.contourf(xx, yy, Z, cmap=plt.get_cmap('YlOrRd'), alpha=.9,levels=levels)
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], 
+                alpha=.5, 
+                s=70, 
+                edgecolors='none', 
+                c = 'white',
+                cmap = plt.get_cmap('YlOrRd'))
+    #labels
+    for id in range( X_reduced.shape[0] ):
+        if labels is None:
+            label = str(id) 
+        else:
+            label = labels[id] 
+        x = X_reduced[id, 0]
+        y = X_reduced[id, 1]
+        plt.annotate(label,xy = (x,y), xytext = (0, 0), textcoords = 'offset points')
+    plt.show()
