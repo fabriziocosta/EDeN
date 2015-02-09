11e013b2aed5a718e0e8cd98218b85fbea6e5ede
Fabrizio Costa <xfcosta@gmail.com>
2015-02-09 18:10:43 +0100

    examples

--- eden/converter/rna/rnashapes_struct.py
+++ eden/converter/rna/rnashapes_struct.py
@@ -4,6 +4,7 @@ import networkx as nx
 import subprocess as sp
 from eden.modifier.fasta import fasta_to_fasta
 from eden.converter.fasta import seq_to_networkx
+import math
 
 
 def rnashapes_wrapper(sequence, **options):
@@ -11,12 +12,34 @@ def rnashapes_wrapper(sequence, **options):
     shape_type =  options.get('shape_type',5)
     energy_range =  options.get('energy_range',10)
     max_num =  options.get('max_num',3)
+    shape = options.get('shape',False)
+    energy = options.get('energy',False)
+    dotbracket = options.get('dotbracket',True)
     #command line
     cmd = 'echo "%s" | RNAshapes -t %d -c %d -# %d' % (sequence,shape_type,energy_range,max_num)
     out = sp.check_output(cmd, shell = True)
     text = out.strip().split('\n')
     seq_info = text[0]
-    seq_struct_list = [line.split()[1] for line in text[1:-1]] 
+    seq_struct_list = []
+    if shape:
+        #extract the shape bracket notation
+        seq_struct_list += [line.split()[2] for line in text[1:-1]] 
+    if energy:
+        #convert negative energy into positive integer
+        #create a string of length equal to the energy
+        for line in text[1:-1]:
+            energy = int(math.floor(-float(line.split()[0])))
+            energy_dec = int( energy / 10 )
+            reminder = energy % 10
+            energy_string = ""
+            for i in range(energy_dec):
+                energy_string += str(i)*10
+            for i in range(reminder): 
+                energy_string += 'x'
+            seq_struct_list += [energy_string] 
+    if dotbracket:
+        #extract the dot bracket notation
+        seq_struct_list += [line.split()[1] for line in text[1:-1]] 
     return seq_info, seq_struct_list
 
 
@@ -26,26 +49,18 @@ def string_to_networkx(sequence, **options):
     for seq_struct in seq_struct_list:
         G = nx.Graph()
         lifo = list()
-        for i,(c,b) in enumerate( zip(seq_info, seq_struct) ):
+        for i,b in enumerate( seq_struct ):
             G.add_node(i)
-            G.node[i]['label'] = c
+            G.node[i]['label'] = b
             G.node[i]['position'] = i
             if i > 0:
                 G.add_edge(i,i-1)
                 G.edge[i][i-1]['label'] = '-'
-                G.edge[i][i-1]['type'] = 'backbone'
-            if b == '(':
-                lifo += [i]
-            if b == ')':
-                j = lifo.pop()
-                G.add_edge(i,j)
-                G.edge[i][j]['label'] = '='
-                G.edge[i][j]['type'] = 'basepair'
         G_global = nx.disjoint_union(G_global, G)
     return G_global
 
 
-def rnashapes_to_eden(input, **options):
+def rnashapes_struct_to_eden(input, **options):
     lines =  fasta_to_fasta(input)
     for line in lines:
         header = line
