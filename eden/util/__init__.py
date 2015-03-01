7defef3a1eff4f8cc0f6f28c304c50b3bfcd9afc
Fabrizio Costa <xfcosta@gmail.com>
2015-03-01 15:48:09 +0100

    examples

--- eden/util/__init__.py
+++ eden/util/__init__.py
@@ -6,6 +6,7 @@ import os
 from sklearn.linear_model import SGDClassifier
 from sklearn.grid_search import RandomizedSearchCV
 from sklearn import cross_validation
+from sklearn.metrics import classification_report, roc_auc_score, average_precision_score
 from scipy.stats import randint
 from scipy.stats import uniform
 import numpy as np
@@ -13,6 +14,10 @@ from scipy import stats
 from scipy.sparse import vstack
 from itertools import tee
 import random
+
+
+def describe(X):
+    print 'Instances: %d ; Features: %d with an avg of %d features per instance' % (X.shape[0], X.shape[1],  X.getnnz()/X.shape[0])
 
 
 def size(iterable):
@@ -103,6 +108,75 @@ def fit_estimator(positive_data_matrix=None, negative_data_matrix=None, target=N
     print '-' * 80
 
     return optpredictor
+
+
+def fit(iterable_pos_train, iterable_neg_train, vectorizer, n_jobs=1, cv=10):
+    X_pos_train = vectorizer.transform( iterable_pos_train, n_jobs=n_jobs )
+    X_neg_train = vectorizer.transform( iterable_neg_train, n_jobs=n_jobs )
+    #optimize hyperparameters classifier
+    optpredictor = fit_estimator(positive_data_matrix=X_pos_train, negative_data_matrix=X_neg_train, cv=cv, n_jobs=n_jobs)
+    return optpredictor
+
+
+def estimate(iterable_pos_test, iterable_neg_test, estimator, vectorizer, n_jobs=1):
+    X_pos_test = vectorizer.transform( iterable_pos_test, n_jobs=n_jobs )
+    X_neg_test = vectorizer.transform( iterable_neg_test, n_jobs=n_jobs )
+    yp =  [1] * X_pos_test.shape[0]
+    yn = [-1] * X_neg_test.shape[0]
+    y = np.array(yp + yn)
+    X_test = vstack( [X_pos_test,X_neg_test] , format = "csr")
+    print 'Test set'
+    describe(X_test)
+    print '-'*80
+    print 'Test Estimate'
+    predictions=estimator.predict(X_test)
+    margins=estimator.decision_function(X_test)
+    print classification_report(y, predictions)
+    roc = roc_auc_score(y, margins)
+    print 'ROC: %.3f' % roc
+    apr = average_precision_score(y, margins)
+    print 'APR: %.3f'% apr
+    
+
+def self_training(iterable_pos, iterable_neg, vectorizer=None, pos2neg_ratio=0.1, num_iterations=2,  threshold=0,  mode='less_than', n_jobs=-1):
+    def select_ids(predictions, threshold, mode, desired_num_neg):
+        ids = list()
+        for i, prediction in enumerate(predictions):
+            if mode == 'less_then':
+                comparison = prediction < float(threshold)
+            else:
+                comparison = prediction > float(threshold)
+            if comparison:
+                ids.append(i)
+        #keep a random sample of num_neg difficult cases
+        random.shuffle(ids)
+        ids = ids[:desired_num_neg]
+        return ids
+
+    Xpos = vectorizer.transform( iterable_pos, n_jobs=n_jobs )
+    print 'Positives:'
+    describe(Xpos)
+    #select a fraction for the negatives
+    num_pos = Xpos.shape[0]
+    desired_num_neg = int(float(num_pos) * pos2neg_ratio)
+    #select the initial ids for the negatives as the first num_neg
+    ids = range(desired_num_neg)
+    #iterate: select negatives and create a model using postives + selected negatives 
+    for i in range(num_iterations):
+        print 'Iteration: %d/%d'%(i+1,num_iterations)
+        #select only a fraction of the negatives
+        iterable_neg, iterable_neg_copy1, iterable_neg_copy2 = tee(iterable_neg,3)
+        Xneg = vectorizer.transform( selection_iterator(iterable_neg_copy1,ids), n_jobs=n_jobs )
+        print 'Negatives:'
+        describe(Xneg)
+        #fit the estimator on all positives and selected negatives
+        from eden.util import fit_estimator
+        estimator = fit_estimator( positive_data_matrix=Xpos, negative_data_matrix=Xneg, cv=10 )
+        if i < num_iterations -1:
+            #use the estimator to select the next batch of negatives
+            predictions = vectorizer.predict(iterable_neg_copy2, estimator)
+            ids = select_ids(predictions, threshold, mode, desired_num_neg)
+    return estimator
 
 
 def read(uri):
