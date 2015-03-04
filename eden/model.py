ad9028a842e27179d54239a1215879aa75f04623
Fabrizio Costa <xfcosta@gmail.com>
2015-03-04 11:55:18 +0100

    examples

--- eden/model.py
+++ eden/model.py
@@ -17,13 +17,14 @@ from eden.graph import Vectorizer
 
 class BinaryClassificationModel(object):
 
-    def __init__(self, pre_processor=None, vectorizer=Vectorizer(complexity=1), estimator=SGDClassifier(class_weight='auto', shuffle=True)):
+    def __init__(self, pre_processor=None, vectorizer=Vectorizer(complexity=1), estimator=SGDClassifier(class_weight='auto', shuffle=True), random_seed=1):
         self.pre_processor = pre_processor
         self.vectorizer = vectorizer
         self.estimator = estimator
         self.pre_processor_args = None
         self.vectorizer_args = None
         self.estimator_args = None
+        random.seed(random_seed)
 
     def save(self, model_name):
         joblib.dump(self, model_name, compress=1)
@@ -63,6 +64,9 @@ class BinaryClassificationModel(object):
         iterator_neg = self.pre_processor(
             iterable_neg, **self.pre_processor_args)
         Xneg = self.vectorizer.transform(iterator_neg, n_jobs=n_jobs)
+        return self._assemble_data_matrix(Xpos, Xneg)
+
+    def _assemble_data_matrix(self, Xpos, Xneg):
         yp = [1] * Xpos.shape[0]
         yn = [-1] * Xneg.shape[0]
         y = np.array(yp + yn)
@@ -86,8 +90,16 @@ class BinaryClassificationModel(object):
                  n_jobs=1,
                  cv=10,
                  scoring='roc_auc'):
+        def print_args():
+            print('Current parameters:')
+            print('Pre_processor:')
+            pprint.pprint(self.pre_processor_args)
+            print('Vectorizer:')
+            pprint.pprint(self.vectorizer_args)
+            print('Estimator:')
+            pprint.pprint(self.estimator_args)
         if verbose:
-            print('Paramters range')
+            print('Parameters range:')
             print('Pre_processor:')
             pprint.pprint(pre_processor_parameters)
             print('Vectorizer:')
@@ -129,12 +141,7 @@ class BinaryClassificationModel(object):
                     print e.__doc__
                     print e.message
                     print('Failed with the following setting:')
-                    print('Pre_processor:')
-                    pprint.pprint(self.pre_processor_args)
-                    print('Vectorizer:')
-                    pprint.pprint(self.vectorizer_args)
-                    print('Estimator:')
-                    pprint.pprint(self.estimator_args)
+                    print_args()
                     print '...continuing'
             else:
                 # consider as score the mean-std for a robust estimate
@@ -155,12 +162,7 @@ class BinaryClassificationModel(object):
                               (i + 1, n_iter, time.time() - start))
                         print('Best score (%s): %f (%f +- %f)' %
                               (scoring, best_score_, best_score_mean_, best_score_std_))
-                        print('Pre_processor:')
-                        pprint.pprint(self.pre_processor_args)
-                        print('Vectorizer:')
-                        pprint.pprint(self.vectorizer_args)
-                        print('Estimator:')
-                        pprint.pprint(self.estimator_args)
+                        print_args()
                         print 'Instances: %d ; Features: %d with an avg of %d features per instance' % (X.shape[0], X.shape[1],  X.getnnz() / X.shape[0])
                         print report_base_statistics(y)
         # store the best hyperparamter configuration
@@ -184,6 +186,8 @@ class BinaryClassificationModel(object):
         return self.estimator.decision_function(X)
 
     def estimate(self, iterable_pos, iterable_neg, n_jobs=1, cv=10, plots=False):
+        iterable_pos, iterable_pos_ = tee(iterable_pos)
+        iterable_pos, iterable_pos_ = tee(iterable_pos)
         X, y = self._data_matrices(iterable_pos, iterable_neg, n_jobs=n_jobs)
         print 'Classifier:'
         print self.estimator
@@ -199,21 +203,25 @@ class BinaryClassificationModel(object):
         print '-' * 80
 
 
-class SelfTrainingBinaryClassificationModel(BinaryClassificationModel):
+class ActiveLearningBinaryClassificationModel(BinaryClassificationModel):
 
-    def __init__(self, pre_processor=None, vectorizer=Vectorizer(complexity=1), estimator=SGDClassifier(class_weight='auto', shuffle=True)):
+    def __init__(self, pre_processor=None, vectorizer=Vectorizer(complexity=1), estimator=SGDClassifier(class_weight='auto', shuffle=True), random_seed=1):
         self.pre_processor = pre_processor
         self.vectorizer = vectorizer
         self.estimator = estimator
         self.pre_processor_args = None
         self.vectorizer_args = None
         self.estimator_args = None
+        random.seed(random_seed)
 
     def optimize(self, iterable_pos, iterable_neg,
-                 neg_to_pos_ratio=0.1,
-                 num_selftraining_iterations=2,
-                 lower_bound_threshold=-1,
-                 upper_bound_threshold=1,
+                 n_active_learning_iterations=2,
+                 size_positive=-1,
+                 size_negative=-1,
+                 lower_bound_threshold_positive=-1,
+                 upper_bound_threshold_positive=1,
+                 lower_bound_threshold_negative=-1,
+                 upper_bound_threshold_negative=1,
                  n_iter=20,
                  pre_processor_parameters=dict(),
                  vectorizer_parameters=dict(),
@@ -222,8 +230,16 @@ class SelfTrainingBinaryClassificationModel(BinaryClassificationModel):
                  n_jobs=1,
                  cv=10,
                  scoring='roc_auc'):
+        def print_args():
+            print('Current parameters:')
+            print('Pre_processor:')
+            pprint.pprint(self.pre_processor_args)
+            print('Vectorizer:')
+            pprint.pprint(self.vectorizer_args)
+            print('Estimator:')
+            pprint.pprint(self.estimator_args)
         if verbose:
-            print('Paramters range')
+            print('Parameters range:')
             print('Pre_processor:')
             pprint.pprint(pre_processor_parameters)
             print('Vectorizer:')
@@ -255,10 +271,13 @@ class SelfTrainingBinaryClassificationModel(BinaryClassificationModel):
                     iterable_pos, iterable_pos_ = tee(iterable_pos)
                     iterable_neg, iterable_neg_ = tee(iterable_neg)
                     X, y = self._self_training_data_matrices(iterable_pos_, iterable_neg_,
-                                                             neg_to_pos_ratio=neg_to_pos_ratio,
-                                                             num_selftraining_iterations=num_selftraining_iterations,
-                                                             lower_bound_threshold=lower_bound_threshold,
-                                                             upper_bound_threshold=upper_bound_threshold,
+                                                             n_active_learning_iterations=n_active_learning_iterations,
+                                                             size_positive=size_positive,
+                                                             size_negative=size_negative,
+                                                             lower_bound_threshold_positive=lower_bound_threshold_positive,
+                                                             upper_bound_threshold_positive=upper_bound_threshold_positive,
+                                                             lower_bound_threshold_negative=lower_bound_threshold_negative,
+                                                             upper_bound_threshold_negative=upper_bound_threshold_negative,
                                                              n_jobs=n_jobs)
                 scores = cross_validation.cross_val_score(
                     self.estimator, X, y, cv=cv, scoring=scoring, n_jobs=n_jobs)
@@ -269,12 +288,7 @@ class SelfTrainingBinaryClassificationModel(BinaryClassificationModel):
                     print e.__doc__
                     print e.message
                     print('Failed with the following setting:')
-                    print('Pre_processor:')
-                    pprint.pprint(self.pre_processor_args)
-                    print('Vectorizer:')
-                    pprint.pprint(self.vectorizer_args)
-                    print('Estimator:')
-                    pprint.pprint(self.estimator_args)
+                    print_args()
                     print '...continuing'
             else:
                 # consider as score the mean-std for a robust estimate
@@ -295,12 +309,7 @@ class SelfTrainingBinaryClassificationModel(BinaryClassificationModel):
                               (i + 1, n_iter, time.time() - start))
                         print('Best score (%s): %f (%f +- %f)' %
                               (scoring, best_score_, best_score_mean_, best_score_std_))
-                        print('Pre_processor:')
-                        pprint.pprint(self.pre_processor_args)
-                        print('Vectorizer:')
-                        pprint.pprint(self.vectorizer_args)
-                        print('Estimator:')
-                        pprint.pprint(self.estimator_args)
+                        print_args()
                         print 'Instances: %d ; Features: %d with an avg of %d features per instance' % (X.shape[0], X.shape[1],  X.getnnz() / X.shape[0])
                         print report_base_statistics(y)
         # store the best hyperparamter configuration
@@ -310,42 +319,68 @@ class SelfTrainingBinaryClassificationModel(BinaryClassificationModel):
         # fit the estimator using the best hyperparameters
         self.fit(iterable_pos, iterable_neg, n_jobs=n_jobs)
 
-    def _self_training_data_matrices(self, iterable_pos, iterable_neg, neg_to_pos_ratio=0.1, num_selftraining_iterations=2,  lower_bound_threshold=-1,  upper_bound_threshold=1, n_jobs=1):
-        Xpos = self._data_matrix(iterable_pos, n_jobs=n_jobs)
-        # select a fraction for the negatives
-        num_pos = Xpos.shape[0]
-        desired_size = int(float(num_pos) * neg_to_pos_ratio)
-        # select the initial ids for the negatives as the first num_neg
-        # occurrences
-        ids = range(desired_size)
-        # iterate: select negatives according to current model score and create
-        # a model using postives and selected negatives
-        for i in range(num_selftraining_iterations):
-            # select only a fraction of the negatives
-            iterable_neg, iterable_neg_, iterable_neg__ = tee(iterable_neg, 3)
-            Xneg = self._data_matrix(
-                selection_iterator(iterable_neg_, ids), n_jobs=n_jobs)
+    def _self_training_data_matrices(self, iterable_pos, iterable_neg,
+                                     n_active_learning_iterations=2,
+                                     size_positive=-1,
+                                     size_negative=100,
+                                     lower_bound_threshold_positive=-1,
+                                     upper_bound_threshold_positive=1,
+                                     lower_bound_threshold_negative=-1,
+                                     upper_bound_threshold_negative=1,
+                                     n_jobs=1):
+        # select the initial ids simply as the first occurrences
+        positive_ids = range(size_positive)
+        negative_ids = range(size_negative)
+        # iterate: select instances according to current model and create novel
+        # data matrix to fit the model in next round
+        for i in range(n_active_learning_iterations):
+            # make data matrix on selected instances
+            # if this is the first iteration or we need to select positives
+            if i == 0 or size_positive != -1:
+                iterable_pos, iterable_pos_, iterable_pos__ = tee(
+                    iterable_pos, 3)
+                if size_positive == -1:  # if we take all positives
+                    Xpos = self._data_matrix(iterable_pos_, n_jobs=n_jobs)
+                else:  # otherwise use selection
+                    Xpos = self._data_matrix(
+                        selection_iterator(iterable_pos_, positive_ids), n_jobs=n_jobs)
+            # if this is the first iteration or we need to select negatives
+            if i == 0 or size_negative != -1:
+                iterable_neg, iterable_neg_, iterable_neg__ = tee(
+                    iterable_neg, 3)
+                if size_negative == -1:  # if we take all negatives
+                    Xneg = self._data_matrix(iterable_neg_, n_jobs=n_jobs)
+                else:  # otherwise use selection
+                    Xneg = self._data_matrix(
+                        selection_iterator(iterable_neg_, negative_ids), n_jobs=n_jobs)
             # assemble data matrix
-            yp = [1] * Xpos.shape[0]
-            yn = [-1] * Xneg.shape[0]
-            y = np.array(yp + yn)
-            X = vstack([Xpos, Xneg], format="csr")
-            # stop fitting a model at the last-1 iteration, just return the
-            # selected matrix
-            if i == num_selftraining_iterations - 1:
+            X, y = self._assemble_data_matrix(Xpos, Xneg)
+            # stop the fitting procedure at the last-1 iteration and return X,y
+            if i == n_active_learning_iterations - 1:
                 break
-            # fit the estimator on all positives and selected negatives
+            # fit the estimator on selected instances
             self.estimator.fit(X, y)
-            # use the estimator to select the next batch of negatives
-            iterator = self.pre_processor(iterable_neg__)
-            predictions = self.vectorizer.predict(iterator, self.estimator)
-            ids = list()
-            for i, prediction in enumerate(predictions):
-                if prediction >= float(lower_bound_threshold) and prediction <= float(upper_bound_threshold):
-                    ids.append(i)
-            if len(ids) == 0:
-                raise Exception('No instances found that satisfy constraints')
-            # keep a random sample of num_neg difficult cases
-            random.shuffle(ids)
-            ids = ids[:desired_size]
+            # use the trained estimator to select the next instances
+            if size_positive != -1:
+                positive_ids = self._bounded_selection(
+                    iterable_pos__, size=size_positive, lower_bound_threshold=lower_bound_threshold_positive, upper_bound_threshold=upper_bound_threshold_positive)
+            if size_negative != -1:
+                negative_ids = self._bounded_selection(
+                    iterable_neg__, size=size_negative, lower_bound_threshold=lower_bound_threshold_negative, upper_bound_threshold=upper_bound_threshold_negative)
         return X, y
+
+    def _bounded_selection(self, iterable, size=None, lower_bound_threshold=None, upper_bound_threshold=None):
+        #transform row data to graphs
+        iterable = self.pre_processor(iterable)
+        #use estimator to predict margin for instances in an out-of-core fashion
+        predictions = self.vectorizer.predict(iterable, self.estimator)
+        ids = list()
+        for i, prediction in enumerate(predictions):
+            if prediction >= float(lower_bound_threshold) and prediction <= float(upper_bound_threshold):
+                ids.append(i)
+        if len(ids) == 0:
+            raise Exception('No instances found that satisfy constraints')
+        # keep a random sample of num_neg difficult cases
+        random.shuffle(ids)
+        ids = ids[:size]
+        return ids
