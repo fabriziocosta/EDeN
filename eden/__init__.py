abfd1c7370043ef76699575e1ee3047c416d53ec
Fabrizio Costa <xfcosta@gmail.com>
2015-04-14 20:17:41 +0200

    added vectorizer in util; removed parallelization in Vectorizer

--- eden/__init__.py
+++ eden/__init__.py
@@ -10,6 +10,35 @@ __status__ = "Production"
 
 from itertools import izip_longest
 
+
+def serial_vectorize(graphs, vectorizer=None):
+    X = vectorizer.transform(graphs)
+    return X
+
+
+def multiprocess_vectorize(graphs, vectorizer=None, negative_ratio=2, n_blocks=5, n_processes=8):
+    graphs = list(graphs)
+    import multiprocessing as mp
+    size = len(graphs)
+    block_size = size / n_blocks
+    pool = mp.Pool(processes=n_processes)
+    results = [pool.apply_async(serial_vectorize, args=(graphs[s * block_size:(s + 1) * block_size], vectorizer)) for s in range(n_blocks - 1)]
+    output = [p.get() for p in results]
+    import numpy as np
+    from scipy.sparse import vstack
+    X = output[0]
+    for Xi in output[1:]:
+        X = vstack([X, Xi], format="csr")
+    return X
+
+
+def vectorize(graphs, vectorizer=None, multiprocess=True, n_blocks=5, n_processes=8):
+    if multiprocess:
+        return multiprocess_vectorize(graphs, vectorizer=vectorizer, n_blocks=n_blocks, n_processes=n_processes)
+    else:
+        return serial_vectorize(graphs, vectorizer=vectorizer)
+
+
 def calc_running_hash(running_hash, list_item, counter):
     return ((~(((running_hash << 11) + list_item) ^ (running_hash >> 5))), ((running_hash << 7) ^ list_item * (running_hash >> 3)))[bool((counter & 1) == 0)]
 
