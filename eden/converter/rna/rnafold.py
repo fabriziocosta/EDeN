7ea8e735a6f87c41c62fa0663824e5aeb768742a
Fabrizio Costa <xfcosta@gmail.com>
2015-05-28 17:33:51 +0200

    cleaned

--- eden/converter/rna/rnafold.py
+++ eden/converter/rna/rnafold.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-import networkx as nx
 import subprocess as sp
-from eden.modifier.fasta import fasta_to_fasta
 from eden.converter.fasta import seq_to_networkx
 from eden.converter.rna import sequence_dotbracket_to_graph
 from eden.util import is_iterable
