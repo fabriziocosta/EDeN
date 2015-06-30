27c683c8161d01d4ba67613a70778a7a20aa1c8a
Fabrizio Costa <xfcosta@gmail.com>
2015-06-30 09:49:59 +0200

    pep8

--- eden/converter/graph/gspan.py
+++ eden/converter/graph/gspan.py
@@ -29,34 +29,34 @@ def gspan_to_networkx(header, lines):
 
     Keyword arguments:
     header -- string to be used as id for the graph
-    lines  -- string list in extended gSpan format 
+    lines  -- string list in extended gSpan format
     """
     # remove empty lines
     lines = [line for line in lines if line.strip()]
-    
+
     graph = nx.Graph(id=header)
     for line in lines:
         tokens = line.split()
         fc = tokens[0]
-        
+
         # process vertices
         if fc in ['v', 'V']:
             id = int(tokens[1])
             label = tokens[2]
             weight = 1.0
-            
+
             # uppercase V indicates no-viewpoint, in the new EDeN this is simulated via a smaller weight
             if fc == 'V':
                 weight = 0.1
-                
+
             graph.add_node(id, ID=id, label=label, weight=weight)
-            
+
             # extract the rest of the line  as a JSON string that contains all attributes
             attribute_str = ' '.join(tokens[3:])
             if attribute_str.strip():
                 attribute_dict = json.loads(attribute_str)
                 graph.node[id].update(attribute_dict)
-                
+
         # process edges
         if fc == 'e':
             src = int(tokens[1])
@@ -67,7 +67,7 @@ def gspan_to_networkx(header, lines):
             if attribute_str.strip():
                 attribute_dict = json.loads(attribute_str)
                 graph.edge[src][dst].update(attribute_dict)
-                    
+
     assert(len(graph) > 0), 'ERROR: generated empty graph. Perhaps wrong format?'
     return graph
 
@@ -82,9 +82,9 @@ def eden_to_gspan(graphs, filename):
     f = open(filename, 'w')
     for i, graph in enumerate(graphs):
         f.write('t # ' + str(i) + '\n')
-        
+
         for node, data in graph.nodes_iter(data=True):
-            f.write('v ' + str(node) + ' ' + data['label'] + '\n') 
-        
+            f.write('v ' + str(node) + ' ' + data['label'] + '\n')
+
         for src, dst, data in graph.edges_iter(data=True):
             f.write('e ' + str(src) + ' ' + str(dst) + ' ' + data['label'] + '\n')
