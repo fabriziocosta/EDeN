b067ea5223f46bc58f1aa5aedee9548f33df4588
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2015-09-09 14:00:03 +0200

    add stub for new test function

--- test/test_graphprot.py
+++ test/test_graphprot.py
@@ -3,3 +3,13 @@ from graphprot import testfun
 
 def test_testfun():
     testfun("hello world!")
+
+
+from eden.converter.fasta import fasta_to_sequence
+from eden.converter.fasta import sequence_to_eden
+
+
+def test_fasta_to_sequence_graph():
+    fa_fn = "test/test_fasta_to_sequence.fa"
+    seq = fasta_to_sequence(fa_fn)
+    gra = sequence_to_eden(seq)
