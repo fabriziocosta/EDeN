7433372769ec64fdbce3b2a9fdd4b100f8539e78
Fabrizio Costa <xfcosta@gmail.com>
2016-01-29 16:28:51 +0100

    cleanup

--- eden/interfaces.py
+++ eden/interfaces.py
@@ -3,21 +3,31 @@ logger = logging.getLogger(__name__)
 
 
 """
-preconditions are programs that ensure that a contract is fulfilled by the input stream.
-postconditions are programs that ensure that a contract is fulfilled by the output stream.
-They are callable functions with input iterable/output iterable and program as parameters that evaluate to
-True or False.
+preconditions are programs that ensure that a condition/contract is fulfilled by the input stream given
+the specific program and parameters_priors.
+preconditions take in input all the input to the interface.
+postconditions are programs that ensure that a condition/contract is fulfilled by the output stream given
+the specific program and parameters_priors.
+postconditions take in input all the input to the interface and the produced output.
+pre/postconditions evaluate to True or False.
 
-E.g. if the precondition is 'inducible' then perform optimization of parameters using parameters_priors,
-unpacking the input in pairs of (data_in, target_out). Otherwise do testing.
+E.g. if the precondition tests the 'inducible' contract then it will check that each data
+instance stores an attribute called 'target' and the program will be adapted via optimization
+on the parameter space sampled using parameters_priors.
+Otherwise do testing.
 
 Provide collections of well documented programs (each with as simple an interface as possible, but not more)
 Organize the collection ob the basis of the interfaces.
 """
 
 
+def convert(iterable, precondition=None, postcondition=None, program=None, parameters_priors=None):
+    """Map an input data type to a graph."""
+    pass
+
+
 def associate(iterable, precondition=None, postcondition=None, program=None, parameters_priors=None):
-    """Map a graph to another, simple data type."""
+    """Map a graph to an output data type."""
     pass
 
 
