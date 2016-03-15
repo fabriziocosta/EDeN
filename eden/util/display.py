875575ef0a3b6454821b70fbff8aeedd7b6de3da
Your Name <you@example.com>
2016-03-15 13:39:51 +0900

    travis

--- eden/util/display.py
+++ eden/util/display.py
@@ -141,7 +141,7 @@ def draw_graph(graph,
         import RNA
         rna_object = RNA.get_xy_coordinates(graph.graph['structure'])
         pos = {i: (rna_object.get(i).X, rna_object.get(i).Y)
-               for i in range(len(graph.graph['structure']))}
+            for i in range(len(graph.graph['structure']))}
     elif layout == 'circular':
         pos = nx.circular_layout(graph)
     elif layout == 'random':
