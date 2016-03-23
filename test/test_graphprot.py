a94f02c439dd1498b37dcacb8a22d88c6dd76c46
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2016-03-23 15:37:03 +0100

    add tests

--- test/test_graphprot.py
+++ test/test_graphprot.py
@@ -1,13 +1,15 @@
 from scripttest import TestFileEnvironment
 import re
+from filecmp import cmp
 
 bindir = "graphprot/"
 script = "graphprot_seqmodel"
 # test file environment
-testdir = "test/testenv_graphprot_seqmodel"
+datadir = "test/"
+testdir = "test/testenv_graphprot_seqmodel/"
 # directories relative to test file environment
 bindir_rel = "../../" + bindir
-datadir_rel = "../../test/"
+datadir_rel = "../../" + datadir
 
 env = TestFileEnvironment(testdir)
 
@@ -20,3 +22,15 @@ def test_invocation_no_params():
         expect_error=True)
     assert run.returncode == 2
     assert re.match("usage", run.stderr), "stderr should contain usage information: {}".format(run.stderr)
+
+
+def test_simple_fit():
+    "Train a model on 10 positive and 10 negative sequences using default paramters."
+    outfile = "test_simple_fit.model"
+    call = bindir_rel + script + " fit -p {} -n {} --output-dir ./ --model-file {} --n-iter 1".format(
+        datadir_rel + "rndseqs_10_a.fa",
+        datadir_rel + "rndseqs_10_b.fa",
+        outfile
+    )
+    run = env.run(call)
+    assert cmp(testdir + outfile, datadir + outfile), "Error: trained models don't match"
