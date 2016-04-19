b1325e1f54b3b06f3389d25f4f28c462e074a352
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2016-04-19 11:29:52 +0200

    improve test script; add (disabled) tests

--- test/test_graphprot.py
+++ test/test_graphprot.py
@@ -59,5 +59,51 @@ def test_simple_fit():
         call,
         expect_stderr=True,
     )
-    stdout = open(testdir + "test_simple_fit_estimate.out", "w")
+    stdout = open(testdir + outfile + ".cv.out", "w")
     stdout.write(run.stdout)
+
+
+# def test_manualparams_fit():
+#     "Train a model on 10 positive and 10 negative sequences setting parameters to non-default values."
+#     outfile = "test_manualparams_fit.model"
+#     call = bindir_rel + script + " -vvv fit -p {} -n {} --output-dir ./ --model-file {} --n-iter 1 --nbits 20".format(
+#         datadir_rel + "PARCLIP_MOV10_Sievers_10seqs.train.positives.fa",
+#         datadir_rel + "PARCLIP_MOV10_Sievers_10seqs.train.negatives.fa",
+#         outfile
+#     )
+#     env.run(call)
+#     call = bindir_rel + script + " -vvv estimate -p {} -n {} --output-dir ./ --model-file {} --cross-validation".format(
+#         datadir_rel + "PARCLIP_MOV10_Sievers_10seqs.train.positives.fa",
+#         datadir_rel + "PARCLIP_MOV10_Sievers_10seqs.train.negatives.fa",
+#         outfile
+#     )
+#     run = env.run(
+#         call,
+#         expect_stderr=True,
+#     )
+#     stdout = open(testdir + outfile + ".cv.out", "w")
+#     stdout.write(run.stdout)
+
+
+# def test_optimized_fit():
+#     "Train a model on 100 positive and 100 negative sequences, optimize paramters."
+#     outfile = "test_optmized_fit.model"
+#     call = bindir_rel + script + " -vvv fit -p {} -n {} --output-dir ./ --model-file {} --n-iter 5".format(
+#         datadir_rel + "PARCLIP_MOV10_Sievers_100seqs.train.positives.fa",
+#         datadir_rel + "PARCLIP_MOV10_Sievers_100seqs.train.negatives.fa",
+#         outfile
+#     )
+#     # ../../graphprot/graphprot_seqmodel -vvv fit -p ../../test/PARCLIP_MOV10_Sievers_100seqs.train.positives.fa -n ../../test/PARCLIP_MOV10_Sievers_100seqs.train.negatives.fa --output-dir ./ --model-file test_optimized_fit.model --n-iter 5
+#     env.run(call)
+#     call = bindir_rel + script + " -vvv estimate -p {} -n {} --output-dir ./ --model-file {} --cross-validation".format(
+#         datadir_rel + "PARCLIP_MOV10_Sievers_100seqs.train.positives.fa",
+#         datadir_rel + "PARCLIP_MOV10_Sievers_100seqs.train.negatives.fa",
+#         outfile
+#     )
+#     # ../../graphprot/graphprot_seqmodel -vvv estimate -p ../../test/PARCLIP_MOV10_Sievers_100seqs.train.positives.fa -n ../../test/PARCLIP_MOV10_Sievers_100seqs.train.negatives.fa --output-dir ./ --model-file test_optimized_fit.model --cross-validation
+#     run = env.run(
+#         call,
+#         expect_stderr=True,
+#     )
+#     stdout = open(testdir + outfile + ".cv.out", "w")
+#     stdout.write(run.stdout)
