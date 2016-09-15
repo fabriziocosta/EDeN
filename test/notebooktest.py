593548ffc56f30532e37db6ef460482e8ae11063
S Mautner <mautner@cs.uni-freiburg.de>
2016-09-15 13:07:48 +0200

    notebooktest

---
+++ test/notebooktest.py
@@ -0,0 +1,19 @@
+import os
+
+
+def test_notebooks():
+    notebooks = ['Nearest_Neighbors_and_Gram_Matrix.ipynb']
+    problems = []
+    for notebook in notebooks:
+        cmd = 'wget https://raw.githubusercontent.com/fabriziocosta/EDeN_examples/master/%s' % notebook
+        res = os.system(cmd)
+        cmd = 'jupyter nbconvert --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 %s' % notebook
+        res = os.system(cmd)
+        os.system('rm -f %s' % notebook)
+
+        assert res == 0
+
+
+test_notebooks()
+
+
