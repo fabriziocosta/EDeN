cae536fde2ae2862f5561b6e5bd44c68ee018b4d
S Mautner <mautner@cs.uni-freiburg.de>
2016-09-15 14:45:41 +0200

    test_notebooks.py
    this should fail the travis and produce less waste in output

--- test/test_notebooks.py
+++ test/test_notebooks.py
@@ -4,9 +4,21 @@ import os
 def test_notebooks():
     notebooks = ['Nearest_Neighbors_and_Gram_Matrix.ipynb']
     for notebook in notebooks:
-        cmd = 'wget https://raw.githubusercontent.com/fabriziocosta/EDeN_examples/master/%s' % notebook
+        cmd = 'wget -q https://raw.githubusercontent.com/fabriziocosta/EDeN_examples/master/%s' % notebook
         os.system(cmd)
-        cmd = 'jupyter nbconvert --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 %s' % notebook
+        cmd = 'jupyter nbconvert  --stdout --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 %s > /dev/null' % notebook
         res = os.system(cmd)
         os.system('rm -f %s' % notebook)
+        #os.system('rm -f %s' % notebook[:])
         assert res == 0
+
+def test_notebooks2():
+    notebook = 'cascade.ipynb'
+    cmd = 'wget -q https://raw.githubusercontent.com/smautner/GraphLearn_examples/master/notebooks/%s'  % notebook
+    os.system(cmd)
+    cmd = 'jupyter nbconvert  --stdout --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 %s > /dev/null' % notebook
+    res = os.system(cmd)
+    os.system('rm -f %s' % notebook)
+    assert res == 0
+
+
