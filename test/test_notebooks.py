a8ef3ed62681fb5123d2525740023e51d9d71e38
S Mautner <mautner@cs.uni-freiburg.de>
2016-09-15 14:51:07 +0200

    test_notebooks.py
    rm failing test, confirmed that travis is checking notebooks indeed

--- test/test_notebooks.py
+++ test/test_notebooks.py
@@ -12,13 +12,3 @@ def test_notebooks():
         #os.system('rm -f %s' % notebook[:])
         assert res == 0
 
-def test_notebooks2():
-    notebook = 'cascade.ipynb'
-    cmd = 'wget -q https://raw.githubusercontent.com/smautner/GraphLearn_examples/master/notebooks/%s'  % notebook
-    os.system(cmd)
-    cmd = 'jupyter nbconvert  --stdout --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 %s > /dev/null' % notebook
-    res = os.system(cmd)
-    os.system('rm -f %s' % notebook)
-    assert res == 0
-
-
