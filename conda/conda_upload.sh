8dde2516f313854690019a6aa4ab93c19b029887
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2018-06-13 15:03:27 +0200

    simplify conda upload

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -4,15 +4,12 @@ USER=dmaticzka
 
 OS=linux-64
 mkdir ~/conda-bld
-conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 # update version from tags
 python setup.py sdist
 export VERSION=`python setup.py --version | grep --only-matching '^[0-9\.]\+'`_`date +%Y.%m.%d`
-conda build . && \
-anaconda \
--t $CONDA_UPLOAD_TOKEN upload \
--u $USER \
--l nightly \
---force \
-$CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-py`echo ${TRAVIS_PYTHON_VERSION} | tr -d '.'`_0.tar.bz2
+conda config --set anaconda_upload yes
+conda config --set force yes
+conda build . \
+--user $USER \
+--token $CONDA_UPLOAD_TOKEN
